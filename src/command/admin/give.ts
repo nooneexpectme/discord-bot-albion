e1f7e7638a5859fe0babce40c0d5d8c522d27da3
nooneexpectme <erwann0danguy@gmail.com>
2018-07-01 11:22:05 +0200

    Create give command

---
+++ src/command/admin/give.ts
@@ -0,0 +1,40 @@
+// Imports
+import { Client, CommandBase } from '@tanuki/discord-bot-base'
+import { Storage } from '../../storage'
+
+// Transformers
+import UserType from '../../transformer/user/UserType'
+import UserValidator from '../../transformer/user/UserValidator'
+
+// Command
+module.exports = class AdminGiveCommand extends CommandBase {
+    public client: Client
+
+    constructor(client: Client) {
+        super(client, {
+            group: 'admin',
+            name: 'give',
+            description: 'Créer une nouvelle donation pour un utilisateur',
+            userIds: client.settings.ownerIds,
+            args: [
+                { name: 'user', type: UserType, validator: UserValidator },
+                { name: 'points', type: Number },
+                { name: 'resources', type: Number, isOptional: true, default: 0 }
+            ]
+        });
+    }
+
+    public async run(msg, { user, points, resources }) {
+        const storage: Storage = this.client.shared.get('storage')
+        const create: any = await storage.donation.create({
+            amount: resources || 0,
+            points,
+            tier: 0,
+            resourceId: null,
+            userId: user.id,
+            timestamp: Date.now()
+        })
+
+        return msg.react(create ? '✅' : ':x:')
+    }
+}
